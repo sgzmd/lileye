c455e5e935f0809fc66ac603af682b59c1d2fdcd
Roman Kirillov <sigizmund@gmail.com>
2025-03-29 09:00:26 +0000

    Some small refactoring, killed tests for now, distracting af

--- app/src/main/java/com/sgzmd/lileye/MainActivity.kt
+++ app/src/main/java/com/sgzmd/lileye/MainActivity.kt
@@ -8,6 +8,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sgzmd.lileye.service.NotificationListener
 
+private const val NOTIFICATION_LISTENER_PREFS = "NotificationListenerPrefs"
+private const val IS_NOTIFICATION_LISTENER_ENABLED = "isNotificationListenerEnabled"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var toggleButton: Button
 
@@ -22,30 +25,55 @@ class MainActivity : AppCompatActivity() {
                 stopNotificationListener()
             } else {
                 requestNotificationPermission()
+                startNotificationListener()
             }
         }
     }
 
-    private fun isNotificationServiceEnabled(): Boolean {
+    private fun hasNotificationPermission(): Boolean {
         val pkgName = packageName
         val flat = Settings.Secure.getString(contentResolver, "enabled_notification_listeners")
         return flat?.contains(pkgName) == true
     }
 
+    private fun isNotificationServiceEnabled(): Boolean {
+        val sharedPreferences = getSharedPreferences(NOTIFICATION_LISTENER_PREFS, MODE_PRIVATE)
+        return sharedPreferences.getBoolean(IS_NOTIFICATION_LISTENER_ENABLED, false)
+    }
+
+    private fun startNotificationListener() {
+        val intent = Intent(this, NotificationListener::class.java)
+        startService(intent)
+
+        updateNotificationListenerSetting(true)
+
+        Toast.makeText(this, "Notification listener started", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun updateNotificationListenerSetting(enabled: Boolean) {
+        val sharedPreferences = getSharedPreferences(NOTIFICATION_LISTENER_PREFS, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean(IS_NOTIFICATION_LISTENER_ENABLED, enabled)
+        editor.apply()
+    }
+
     private fun requestNotificationPermission() {
-        val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
-        startActivity(intent)
-        Toast.makeText(
-            this,
-            "Please enable notification access for LilEye",
-            Toast.LENGTH_LONG
-        ).show()
+        if (!hasNotificationPermission()) {
+            val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
+            startActivity(intent)
+            Toast.makeText(
+                this,
+                "Please enable notification access for LilEye",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 
     private fun stopNotificationListener() {
         val intent = Intent(this, NotificationListener::class.java)
         stopService(intent)
-        toggleButton.text = "Start Listening"
+        updateNotificationListenerSetting(false)
         Toast.makeText(this, "Notification listener stopped", Toast.LENGTH_SHORT).show()
     }
 
